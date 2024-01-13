736724e96cce26b14fbb314b5b5f9154e5d8311f
agboola teslim <teslimagboola09@gmail.com>
2024-01-13 13:29:00 +0100

    my opay commit

--- src/components/DashboardDetails.jsx
+++ src/components/DashboardDetails.jsx
@@ -59,7 +59,7 @@ const DashboardDetails = () => {
                         <p>Transaction Receipt</p>
                     </div>
                 </div>
-                <div className='text-center' style={{ marginTop: "80px" }}>
+                <div className='text-center' style={{ marginTop: "70px" }}>
                     <h2 style={{ color: "green", fontSize: "35px" }}>
                         ₦{amount}.00
                     </h2>
