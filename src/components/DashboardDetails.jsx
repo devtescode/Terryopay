8fc46fbf8f98913d82e290fc1a23d953f404f10d
agboola teslim <teslimagboola09@gmail.com>
2024-01-13 12:47:16 +0100

    my opay commit

---
+++ src/components/DashboardDetails.jsx
@@ -0,0 +1,152 @@
+import React, { useEffect, useState } from 'react'
+import Opay from '../assets/newopay.webp'
+import "./DashboardDetails.css"
+import { useLocation } from 'react-router-dom';
+
+const DashboardDetails = () => {
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const accountName = params.get('accountName');
+    const amount = params.get('amount');
+    const accountnumber = params.get('accountnumber')
+    const Remark = params.get('Remark')
+    const [mydate, setMyDate] = useState('');
+
+    useEffect(() => {
+        const updateDate = () => {
+            const currentDate = new Date().toLocaleString();
+            setMyDate(currentDate);
+        };
+        updateDate();
+    }, []);
+
+    const [randomNumbers, setRandomNumbers] = useState([]);
+    const [randomNumberstwo, setRandomNumberstwo] = useState([]);
+    useEffect(() => {
+        const generateRandomNumbers = () => {
+            const numbers = [];
+            for (let i = 0; i < 9; i++) {
+                const randomNumber = Math.round(Math.random() * 100);
+                numbers.push(randomNumber);
+            }
+            setRandomNumbers(numbers);
+        };
+
+        generateRandomNumbers();
+    }, []);
+
+    useEffect(() => {
+        const generateRandomNumberstwo = () => {
+            const numberstwo = [];
+            for (let i = 0; i < 15; i++) {
+                const randomNumbertwo = Math.round(Math.random() * 100);
+                numberstwo.push(randomNumbertwo);
+            }
+            setRandomNumberstwo(numberstwo);
+        };
+
+        generateRandomNumberstwo();
+    }, []);
+
+    return (
+        <>
+            <div className='border border-2 col-md-5 col-sm-12 mx-auto shadow-lg'>
+                <div className='d-flex justify-content-between mx-2 ' style={{ alignItems: "center" }}>
+                    <div>
+                        <img style={{ width: "80px" }} src={Opay} alt="" />
+                    </div>
+                    <div className='mt-3'>
+                        <p>Transaction Receipt</p>
+                    </div>
+                </div>
+                <div className='text-center' style={{ marginTop: "80px" }}>
+                    <h2 style={{ color: "green", fontSize: "40px" }}>
+                        ₦{amount}.00
+                    </h2>
+                    <h2 className='mimsize'>Success</h2>
+                </div>
+                <div id="showdate" className="text-center" style={{ fontSize: '14px' }}>
+                    {mydate}
+                </div>
+                <div className='mx-2'>
+
+                    <hr />
+                </div>
+                <div className='d-flex justify-content-between mx-2'>
+                    <div>
+                        <p className='putOpacity'>Transaction Type</p>
+                    </div>
+                    <div>
+                        <b>Transfer to bank</b>
+                    </div>
+                </div>
+                <div className='d-flex justify-content-between mx-2'>
+                    <div>
+                        <p className='putOpacity'>Recipient Details</p>
+                    </div>
+                    <div>
+                        <b>{accountName}</b>
+                    </div>
+                </div>
+                <div className='text-end mx-2' style={{ marginTop: "-15px" }}>
+                    <p>{accountnumber}</p>
+                </div>
+                <div className='d-flex justify-content-between mx-2'>
+                    <div>
+                        <p className='putOpacity'>Sender Details</p>
+                    </div>
+                    <div>
+                        <b>TESLIM AYODELE AGBOOLA</b>
+                    </div>
+                </div>
+                <div className='text-end mx-2' style={{ marginTop: "-15px" }}>
+                    <p>Opay | 901****345</p>
+                </div>
+                <div className='d-flex justify-content-between mx-2'>
+                    <div>
+                        <p className='putOpacity'>Remark</p>
+                    </div>
+                    <div>
+                        <p>{Remark}</p>
+                    </div>
+                </div>
+                <div className='d-flex justify-content-between mx-2'>
+                    <div>
+                        <p className='putOpacity'>Transaction Reference</p>
+                    </div>
+                    <div className='d-flex'>
+                        {randomNumbers.map((number, index) => (
+                            <p key={index}>{number}</p>
+                        ))}
+                    </div>
+                </div>
+                <div className='d-flex justify-content-between mx-2'>
+                    <div>
+                        <p className='putOpacity'>SessionID</p>
+                    </div>
+                    <div className='d-flex'>
+                        {randomNumberstwo.map((numbertwo, index) => (
+                            <p key={index}>{numbertwo}</p>
+                        ))}
+                    </div>
+                </div>
+
+                <div class="text-center" style={{ marginTop: "40px" }}>
+                    <p>Support</p>
+                    <p style={{ color: "green", marginTop: "-15px" }}>customerservice@opayinc.com</p>
+                </div>
+                <div className='mt-4 mx-4'>
+                    <hr />
+                </div>
+                <div className='text-center'>
+                    <p>Enjoy a reliable & stable network service and a welcome bonus of ₦1,200 cashback on Opay.</p>
+                </div>
+            </div>
+            <div className='border border-2 col-md-5 col-sm-12 mx-auto shadow-lg mt-4'>
+                <button className='btn btn-success w-100 p-3'>Share Receipt</button>
+            </div>
+        </>
+    )
+}
+
+export default DashboardDetails
---
+++ src/components/Details.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react'
+import "./Details.css"
+import Data from './Data.json'
+import * as Yup from "yup"
+import { useFormik } from 'formik'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+const Details = () => {
+    const [account, setaccount] = useState('')
+    const navigate = useNavigate()
+    const formik = useFormik({
+        initialValues: {
+            accountnumber: "",
+            selectaccount: "",
+            Amount: "",
+            Remark: "",
+        },
+        validationSchema: Yup.object({
+            accountnumber: Yup.number().typeError('Must be a number').required('Required'),
+            selectaccount: Yup.string().required('Required'),
+            Amount: Yup.string().required('Required'),
+            Remark: Yup.string().required('Required'),
+        }),
+        onSubmit: values => {
+            // http://localhost:5000
+            axios.post("https://candyopay.onrender.com/userinvest/userDetails", {AccountNumber: values.accountnumber, Bankcode: values.selectaccount, bank: Data.banks.find((item)=>(item.code=== values.selectaccount)).name})
+            .then((response)=>{
+                swal({
+                    title: "",
+                    text: response.data.message,
+                    icon: "warning",
+                    button: "Aww yiss!",
+                });
+                // alert(response.data.message)
+                if (response.data.status == true){
+                    setaccount(response.data.accountName)          
+                    // alert(response.data.message)
+                    swal({
+                        title: "Good job!",
+                        text: response.data.message,
+                        icon: "success",
+                        button: "Okay",
+                    }); 
+                    // navigate("/check")
+                    navigate(`/check?accountName=${response.data.accountName}&amount=${values.Amount}&accountnumber=${values.accountnumber}&Remark=${values.Remark}`);
+                    console.log(response);
+                }
+            })      
+            .catch((err) => {
+                console.error('Error occurred', err);
+                swal({
+                    title: "",
+                    text: "Incorrect Details",
+                    icon: "warning",
+                    button: "Aww yiss!",
+                });
+                // alert(response.data.message)
+            });
+        }
+    })
+    return (
+        <>
+            <form onSubmit={formik.handleSubmit}>
+                <div className='col-md-6 mx-auto shadow-lg p-3 border border-2 border-dark rounded-3'>
+                    <h4 className='fw-bold text-center'>Receipt</h4>
+                    <div class="input-container">
+                        <input onChange={formik.handleChange} name='Amount' value={formik.values.Amount} placeholder="Amount" class="input-field" type="text" />
+                        <label for="input-field" class="input-label">Amount</label>
+                        <span class="input-highlight"></span>
+                    </div>
+                    <div className='text-end text-danger'>
+                        {formik.errors.Amount}
+                    </div>
+                    <div class="input-container">
+                        <input onChange={formik.handleChange} name='accountnumber' value={formik.values.accountnumber} placeholder="Account Number" class="input-field" type="text" />
+                        <label for="input-field" class="input-label">Account Number</label>
+                        <span class="input-highlight"></span>
+                    </div>
+                    <div className='text-end text-danger'>
+                        {formik.errors.accountnumber}
+                    </div>
+
+                    <select name="selectaccount" onChange={formik.handleChange} value={formik.values.selectaccount} class="my-5 form-select form-select-lg" aria-label="Large select example">
+                        {Data.banks.map((item, index) => (
+                            <option value={item.code}>{item.name}</option>
+                        ))}
+                    </select>
+                    <div className='text-end text-danger'>
+                        {formik.errors.selectaccount}
+                    </div>
+
+                    <div class="input-container">
+                        <input onChange={formik.handleChange} name='Remark' value={formik.values.Remark} placeholder="Remark" class="input-field" type="text" />
+                        <label for="input-field" class="input-label">Remark</label>
+                        <span class="input-highlight"></span>
+                    </div>
+                    <div className='text-end text-danger'>
+                        {formik.errors.Remark}
+                    </div>
+
+                    <div class="input-container my-4">
+                        <input disabled placeholder="Account Name" class="input-field text-center fw-bold" type="text" value={account}/>
+                        <span class="input-highlight"></span>
+                    </div>
+                    <div className='text-center'>
+                        <button className='btn btn-secondary' type="submit">Click On</button>
+                    </div>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default Details
