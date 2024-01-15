280d6459947bb666225a0287f1539c79e6f36245
agboola teslim <teslimagboola09@gmail.com>
2024-01-15 10:25:24 +0100

    repush

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -62,7 +62,7 @@ const Details = () => {
     return (
         <>
             <form onSubmit={formik.handleSubmit}>
-                <div className='col-md-6 mx-auto shadow-lg p-3 border border-2 border-dark rounded-3'>
+                <div className='col-md-6 mx-auto shadow-lg p-3 border border-2 border-dark rounded-3 centerstyle'>
                     <h4 className='fw-bold text-center'>Receipt</h4>
                     <div class="input-container">
                         <input onChange={formik.handleChange} name='Amount' value={formik.values.Amount} placeholder="Amount" class="input-field" type="text" />
