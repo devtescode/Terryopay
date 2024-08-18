8f4f73eb9e49ce27cbd5186141329434e712ad7b
TESCODE <teslimagboola09@gmail.com>
2024-08-18 13:00:07 +0100

    PWA

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: '/public/icons/opay.jpg',
+            src: '/icons/opay.jpg',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/public/icons/opay.jpg',
+            src: '/icons/opay.jpg',
             sizes: '512x512',
             type: 'image/png',
           },
