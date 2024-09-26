b84db3db6464404ae3933c252dfb4ca5ff435cb9
TESCODE <teslimagboola09@gmail.com>
2024-09-26 13:13:39 +0100

    changing the route navigate

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js').then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        // console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, err => {
         console.log('ServiceWorker registration failed: ', err);
       });
